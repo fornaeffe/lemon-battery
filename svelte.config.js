87c7f12167ba14022b8582173f8a590ee0c1b972
Luca Fornasari <fornaeffe@gmail.com>
2026-03-24 23:39:42 +0100

    prerender 2

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 		adapter: adapter()
 	},
 	paths: {
-		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		base: process.argv.includes('dev') ? '' : (process.env.BASE_PATH ?? '')
 	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
